196d78ac199ef90e775b1f3cffe276c134666879
PapaLabber <ejuhl24@student.aau.dk>
2024-11-28 15:05:55 +0100

    Added everything from old repo development

---
+++ src/function-library.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIBRARY_H
+#define FUNCTION_LIBRARY_H
+
+#endif //FUNCTION_LIBRARY_H
+
+void read_map_test();
+void parse_map_test();
+void process_map_test();
---
+++ src/map_processing/read_map.c
@@ -0,0 +1,17 @@
+//
+// Created by thorp on 22/11/2024.
+//
+
+
+// SUGGESTIONS FOR FUNCTIONALITIES
+// Opens the file in read mode.
+// Reads the entire content as a string or list of lines.
+// Handle file errors (e.g. file not found).
+// Return raw text data for further processing.
+
+#include <stdio.h>
+
+void read_map_test() {
+    printf("read_map works\n");
+
+}
